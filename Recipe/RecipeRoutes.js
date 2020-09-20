3506b63a862b0778fa0077075696a5d20f0fdeb1
Jason Nelson <jason.nelson@infocss.com>
2020-09-19 23:41:32 -0400

    Added logging to the routes with the timestamp

--- Recipe/RecipeRoutes.js
+++ Recipe/RecipeRoutes.js
@@ -3,22 +3,47 @@ const Recipe = require('./RecipeFunctions');
 module.exports = function (app) {
 
     app.get("/recipe/ingredients/:ingredients", async function (req, res) {
-        res.send(await Recipe.GetRecipeByIngredients(req.header('token'),req.params.ingredients))
+        let response = await Recipe.GetRecipeByIngredients(req.header('token'), req.params.ingredients);
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response);
     })
     app.get("/recipe/name/:name", async function (req, res) {
-        res.send(await Recipe.GetRecipeByName(req.header('token'),req.params.name))
+        let response = await Recipe.GetRecipeByName(req.header('token'), req.params.name);
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response);
     })
     app.get("/recipe/:recipeId", async function (req, res) {
-        res.send(await Recipe.GetRecipeById(req.header('token'),req.params.recipeId))
+        let response = await Recipe.GetRecipeById(req.header('token'), req.params.recipeId);
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response);
     })
     app.get("/ingredients", async function (req, res) {
-        res.send(await Recipe.GetIngredients(req.header('token')))
+        let response = await Recipe.GetIngredients(req.header('token'));
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response);
     })
     app.post("/ingredients/missing", async function (req, res) {
         var token = req.header('token');
         ingredient = req.body.ingredient;
         username = req.body.username;
         firstName = req.body.firstName;
-        res.send(await Recipe.MissingIngredient(token,ingredient,username,firstName))
+        let response = await Recipe.MissingIngredient(token, ingredient, username, firstName);
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response);
     })
 }
--- User/UserRoutes.js
+++ User/UserRoutes.js
@@ -5,18 +5,33 @@ module.exports = function (app) {
     app.post("/login/", async function (req, res) {
         username = req.body.username;
         password = req.body.password;
-        res.send(await User.Login(username, password))
+        let response = await User.Login(username, password)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.post("/signup/", async function (req, res) {
         firstName = req.body.firstName;
         username = req.body.username;
         password = req.body.password;
-        res.send(await User.SignUp(firstName, username, password))
+        let response = await User.SignUp(firstName, username, password)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.get("/info/", async function (req, res) {
-        res.send(await User.Info(req.header('token')))
+        let response = await User.Info(req.header('token'))
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.post("/saved/", async function (req, res) {
@@ -25,24 +40,44 @@ module.exports = function (app) {
         recipeName = req.body.recipeName;
         recipeSummary = req.body.recipeSummary;
         recipeComments = req.body.recipeComments;
-        res.send(await User.AddRecipe(token, recipeName, recipeId, recipeSummary, recipeComments))
+        let response = await User.AddRecipe(token, recipeName, recipeId, recipeSummary, recipeComments)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.put("/saved/", async function (req, res) {
         var token = req.header('token');
         recipeId = req.body.recipeId;
         recipeComments = req.body.recipeComments;
-        res.send(await User.EditRecipe(token, recipeId, recipeComments))
+        let response = await User.EditRecipe(token, recipeId, recipeComments)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.delete("/saved/", async function (req, res) {
         var token = req.header('token');
-        res.send(await User.DeleteRecipe(token, req.body.recipeId))
+        let response = await User.DeleteRecipe(token, req.body.recipeId)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.get("/custom/:recipeId", async function (req, res) {
         var token = req.header('token');
-        res.send(await User.GetCustomRecipe(token, req.params.recipeId))
+        let response = await User.GetCustomRecipe(token, req.params.recipeId)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.post("/custom/", async function (req, res) {
@@ -52,7 +87,12 @@ module.exports = function (app) {
         recipeInstructions = req.body.recipeInstructions;
         recipeSummary = req.body.recipeSummary;
         recipeComments = req.body.recipeComments;
-        res.send(await User.AddCustomRecipe(token, recipeName, recipeIngredients, recipeInstructions, recipeSummary, recipeComments))
+        let response = await User.AddCustomRecipe(token, recipeName, recipeIngredients, recipeInstructions, recipeSummary, recipeComments)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.put("/custom/", async function (req, res) {
@@ -63,19 +103,34 @@ module.exports = function (app) {
         recipeInstructions = req.body.recipeInstructions;
         recipeSummary = req.body.recipeSummary;
         recipeComments = req.body.recipeComments;
-        res.send(await User.EditCustomRecipe(token, recipeId, recipeName, recipeIngredients, recipeInstructions, recipeSummary, recipeComments))
+        let response = await User.EditCustomRecipe(token, recipeId, recipeName, recipeIngredients, recipeInstructions, recipeSummary, recipeComments)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.delete("/custom/", async function (req, res) {
         var token = req.header('token');
         recipeId = req.body.recipeId;
-        res.send(await User.DeleteCustomRecipe(token, recipeId))
+        let response = await User.DeleteCustomRecipe(token, recipeId)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 
     app.post("/forgot/", async function (req, res) {
         firstName = req.body.firstName;
         username = req.body.username;
         suggestedPassword = req.body.suggestedPassword;
-        res.send(await User.ForgetPassword(firstName, username, suggestedPassword))
+        let response = await User.ForgetPassword(firstName, username, suggestedPassword)
+        var currentTime = new Date();
+        let timestamp = currentTime.getFullYear + '-' + (currentTime.getMonth() + 1) + '-' + currentTime.getDate() + " | " +
+            currentTime.getHours + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+        console.log({ Response: response, Timestamp: timestamp });
+        res.send(response)
     })
 }
